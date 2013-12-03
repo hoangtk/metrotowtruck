00d5bc5ec00edce3ec799663cd613ba03d86a068
John Wang <john.wang@metrospv.com>
2013-12-03 16:59:05 +0800

    Display the CN name in the search list

--- metro_product/product.py
+++ metro_product/product.py
@@ -171,6 +171,50 @@ class product_product(osv.osv):
 			ids = self.search(cr, user, args, limit=limit, context=context)
 		result = self.name_get(cr, user, ids, context=context)
 		return result
+	def name_get(self, cr, user, ids, context=None):
+		if context is None:
+			context = {}
+		if isinstance(ids, (int, long)):
+			ids = [ids]
+		if not len(ids):
+			return []
+		def _name_get(d):
+			name = d.get('name','')
+			code = d.get('default_code',False)
+			if code:
+				name = '[%s] %s' % (code,name)
+			cn_name = d.get('cn_name',False)
+			if cn_name:
+				name = '%s, %s' % (name,cn_name)
+			if d.get('variants'):
+				name = name + ' - %s' % (d['variants'],)
+			return (d['id'], name)
+
+		partner_id = context.get('partner_id', False)
+
+		result = []
+		for product in self.browse(cr, user, ids, context=context):
+			sellers = filter(lambda x: x.name.id == partner_id, product.seller_ids)
+			if sellers:
+				for s in sellers:
+					mydict = {
+							  'id': product.id,
+							  'name': s.product_name or product.name,
+							  'cn_name': product.cn_name,
+							  'default_code': s.product_code or product.default_code,
+							  'variants': product.variants
+							  }
+					result.append(_name_get(mydict))
+			else:
+				mydict = {
+						  'id': product.id,
+						  'name': product.name,
+						  'cn_name': product.cn_name,
+						  'default_code': product.default_code,
+						  'variants': product.variants
+						  }
+				result.append(_name_get(mydict))
+		return result	
 product_product()
 
 
