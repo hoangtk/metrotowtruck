5bf584e64d5e04f6feaa27f220bd1002b597a038
John Wang <john.wang@metrospv.com>
2014-02-27 14:43:11 +0800

    Add the 'No.' to po printing

--- metro_purchase/report/purchase_order_rpt.py
+++ metro_purchase/report/purchase_order_rpt.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#
+#    
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
@@ -15,10 +15,22 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
 #
 ##############################################################################
-import pur_req_rpt
-import purchase_order_rpt
+
+import time
+from openerp.report import report_sxw
+from openerp.osv import osv
+from openerp import pooler
+from openerp.addons.purchase.report.order import order as po_rpt
+def po_repeatIn(self, lst, name,nodes_parent=False):
+    ret_lst = super(po_rpt,self).repeatIn(lst, name,nodes_parent)
+    i = 0
+    for obj in ret_lst:
+        i = i + 1
+        obj.get(name).idx = i
+    return ret_lst
+po_rpt.repeatIn = po_repeatIn
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
