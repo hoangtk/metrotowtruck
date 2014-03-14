2ee80bbabca225f195be0d6aa58eccecee2424ec
John Wang <john.wang@metrospv.com>
2014-03-14 20:04:36 +0800

    Add the help module

---
+++ metro_help/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Metro Help',
+    'version': '1.0',
+    'category': 'Metro',
+    "sequence": 14,
+    'complexity': "easy",
+    'description': """
+    
+        Online help.
+    
+    """,
+    'author': 'Metro Tower Trucks',
+    'website': 'http://www.metrotowtrucks.com',
+    'depends': ['base','email_template'],
+    'init_xml': [],
+    'data' : [
+       "help.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    #web
+    "js": ["static/src/js/help.js"],
+    'qweb' : ["static/src/xml/help.xml"],
+    'css' : ["static/src/css/help.css",],    
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
