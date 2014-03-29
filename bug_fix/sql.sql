8a34be0151ae332c5cfb26a45931353f0a9bdaeb
John Wang <john.wang@metrospv.com>
2014-03-29 18:27:02 +0800

    1.Change the material_request_line.sale_product_id to ondelete restrict. 2.Since the many2many field generated related table refer the target table using ondelete-cascade foreign key, and can not be change by code, use sql to change the foreign key refered to cnc_id_rel table to ondelete restrict, to not allow the sale product id deleting once it is refered by work.order.cnc.sale_product_ids

---
+++ bug_fix/sql.sql
@@ -0,0 +1,3 @@
+--change the work_order_cnc.sale_product_id ondelete to 'restrict'
+alter table "cnc_id_rel" drop CONSTRAINT "cnc_id_rel_id_id_fkey";
+alter table "cnc_id_rel" add CONSTRAINT "cnc_id_rel_id_id_fkey" FOREIGN KEY (id_id) REFERENCES sale_product(id) ON DELETE RESTRICT;
