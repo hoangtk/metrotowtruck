bf3835b3bb1cfb44dcd7680dc160590bc9330799
John Wang <john.wang@metrospv.com>
2014-03-29 19:19:07 +0800

    Do not allow change ID when there are stock move or cnc order related

--- metro_sale/sale_product.py
+++ metro_sale/sale_product.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 from osv import fields,osv
+from openerp.tools.translate import _
 #the ID for the purchase requisition and the material request
 class sale_product(osv.osv):
     _name = "sale.product"
@@ -13,3 +14,20 @@ class sale_product(osv.osv):
     _sql_constraints = [
         ('name_uniq', 'unique(name)', 'ID must be unique!'),
     ]
+    def write(self, cr, uid, ids, vals, context=None):
+        if context is None:
+            context = {}
+        if not ids:
+            return True
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+
+        if 'name' in vals.keys():
+            stock_moves = self.pool.get('material.request.line').search(cr,uid,[('mr_sale_prod_id','in',ids)],context=context)
+            if stock_moves and len(stock_moves) > 0:
+                raise osv.except_osv(_('Warning !'), _("You cannot change the ID which contains stock moving!"))
+            work_order_cncs = self.pool.get('work.order.cnc').search(cr,uid,[('sale_product_ids','in',ids)],context=context)
+            if work_order_cncs and len(work_order_cncs) > 0:
+                raise osv.except_osv(_('Warning !'), _("You cannot change the ID which contains CNC work orders!"))    
+            
+        return super(sale_product, self).write(cr, uid, ids, vals, context=context)
