fc8a817fea1a0c302159f9ec02ca7336f8a0e517
John Wang <john.wang@metrospv.com>
2014-08-15 13:59:42 +0800

    Change MFG task's working hour employee_id field to emp_ids many2many field, allow add more than one employees under one task working hour

--- metro_project/project_mfg.py
+++ metro_project/project_mfg.py
@@ -47,7 +47,7 @@ class mrp_production_workcenter_line(osv.osv):
 class project_task_work(osv.osv):
     _inherit = "project.task.work"
     _columns = {
-        'employee_id': fields.many2one('hr.employee', 'Employee', ondelete='cascade', ),
+        'emp_ids': fields.many2many("hr.employee","task_work_emp","work_id","emp_id",string="Employees"),
         'mfg_ids': fields.many2many('sale.product', 'task_id_rel','task_id','mfg_id',string='MFG IDs',),
     }    
 
@@ -55,8 +55,14 @@ class project_task_work(osv.osv):
         res = super(project_task_work,self).default_get(cr, uid, fields_list, context=context)
         if not res:
             res = {}
-        if "task_id" in  fields_list:
-            res.update({'task_id':11})
+        if 'workorder_id' in context:
+            wo_data = self.pool.get('mrp.production.workcenter.line').browse(cr, uid, context['workorder_id'], context=context)
+            mfg_ids = [mfg_id.id for mfg_id in wo_data.production_id.mfg_ids]
+            res.update({'mfg_ids':mfg_ids})
+        if 'task_employee_ids' in context:
+            #task_employee_ids structure is like: 'task_employee_ids': [[6, False, [392, 391]]
+            res.update({'emp_ids':context['task_employee_ids'][0][2]})
+        return res
     def get_emp_related_details(self, cr, uid, emp_id):
         res = {}
         emp_obj = self.pool.get('hr.employee')
@@ -105,53 +111,33 @@ def _project_timesheet_create(self, cr, uid, vals, context=None):
     if not context:
         context = {}
     if not context.get('no_analytic_entry',False):
-        task_obj = task_obj.browse(cr, uid, vals['task_id'])
-        #check mfg_ids, employee_id, they are required for mfg task working hours
-        if task_obj.project_type == 'mfg':
-            #mfg_ids structure is like: 'mfg_ids': [[6, False, [392, 391]]
-            if len(vals.get('mfg_ids')[0][2]) <= 0:
-                raise osv.except_osv(_('Error!'),_('The MFG IDs are required for the work order task\'s working hour!'))
-            if not vals.get('employee_id',False):
-                raise osv.except_osv(_('Error!'),_('The Employees are required for the work order task\'s working hour!'))
-        #johnw, 08/08/2014, Use the employee to get data, if user set the employee
-        result = {}
-        if vals.get('employee_id',False):
-            result = self.get_emp_related_details(cr, uid, vals.get('employee_id', uid))
-        else:
-            result = self.get_user_related_details(cr, uid, vals.get('user_id', uid))
-            #update the employee_id as the user related employee if user do not supply employee
-            vals['employee_id'] = result['employee_id']
-        #set timesheet's employee_id
-        vals_line['employee_id'] = result['employee_id']
-            
-        vals_line['name'] = '%s: %s' % (tools.ustr(task_obj.name), tools.ustr(vals['name'] or '/'))
-        vals_line['user_id'] = vals['user_id']
-        vals_line['product_id'] = result['product_id']
-        vals_line['date'] = vals['date'][:10]
-
-        # Calculate quantity based on employee's product's uom
-        vals_line['unit_amount'] = vals['hours']
-
-        default_uom = self.pool.get('res.users').browse(cr, uid, uid).company_id.project_time_mode_id.id
-        if result['product_uom_id'] != default_uom:
-            vals_line['unit_amount'] = uom_obj._compute_qty(cr, uid, default_uom, vals['hours'], result['product_uom_id'])
         #johnw, 08/08/2014, Improve the timesheet account analytic generating logic, replace the account analytic account for the mfg task
-        def _add_ana_line():
-            vals_line['account_id'] = acc_id
-            res = timesheet_obj.on_change_account_id(cr, uid, False, acc_id)
-            if res.get('value'):
-                vals_line.update(res['value'])
+        def _add_ana_line(ana_acc_id, employee_id):
+            #set data from employee
+            result = self.get_emp_related_details(cr, uid, employee_id)
+            vals_line['employee_id'] = employee_id
+            vals_line['product_id'] = result['product_id']            
+            vals_line['product_uom_id'] = result['product_uom_id']
             vals_line['general_account_id'] = result['general_account_id']
             vals_line['journal_id'] = result['journal_id']
+            # Calculate quantity based on employee's product's uom
+            if result['product_uom_id'] != default_uom:
+                vals_line['unit_amount'] = uom_obj._compute_qty(cr, uid, default_uom, vals_line['unit_amount'], result['product_uom_id'])
+                        
+            vals_line['account_id'] = ana_acc_id
+            res = timesheet_obj.on_change_account_id(cr, uid, False, ana_acc_id)
+            if res.get('value'):
+                vals_line.update(res['value'])
+                
             vals_line['amount'] = 0.0
-            vals_line['product_uom_id'] = result['product_uom_id']
+            
+            timeline_id = timesheet_obj.create(cr, uid, vals=vals_line, context=context)
+            
             amount = vals_line['unit_amount']
             prod_id = vals_line['product_id']
             unit = False
-            timeline_id = timesheet_obj.create(cr, uid, vals=vals_line, context=context)
-
             # Compute based on pricetype
-            #add the employee_id parameter
+            #add the employee_id parameter, so metro_hr.hr_timesheet.on_change_unit_amount() will calculate cost based on employee setting
             context.update({'employee_id':vals_line['employee_id']})
             amount_unit = timesheet_obj.on_change_unit_amount(cr, uid, timeline_id,
                 prod_id, amount, False, unit, vals_line['journal_id'], context=context)
@@ -159,23 +145,49 @@ def _project_timesheet_create(self, cr, uid, vals, context=None):
                 updv = { 'amount': amount_unit['value']['amount'] }
                 timesheet_obj.write(cr, uid, [timeline_id], updv, context=context)
             vals['hr_analytic_timesheet_id'] = timeline_id
-                        
-        if task_obj.project_type != 'mfg':
-            acc_id = task_obj.project_id and task_obj.project_id.analytic_account_id.id or False
-            _add_ana_line()
-        else:
-            mfg_ids = vals.get('mfg_ids',False)
-            
+                    
+        task_data = task_obj.browse(cr, uid, vals['task_id'])
+        emp_obj = self.pool.get('hr.employee')
+        default_uom = self.pool.get('res.users').browse(cr, uid, uid).company_id.project_time_mode_id.id
+        #check mfg_ids, employee_id, they are required for mfg task working hours
+        if task_data.project_type == 'mfg':
             #mfg_ids structure is like: 'mfg_ids': [[6, False, [392, 391]]
-            if mfg_ids and mfg_ids[0][2] and len(mfg_ids[0][2]) > 0:
-                mfg_ids = mfg_ids[0][2]
-                #split the hours by the count of ID
-                vals_line['unit_amount'] = vals_line['unit_amount']/float(len(mfg_ids))
-                #loop to generate analytic lines by ID list, 
-                for mfg_id in self.pool.get('sale.product').read(cr,uid,mfg_ids,['analytic_account_id'],context=context):
-                    if not mfg_id.get('analytic_account_id'):
-                        continue
-                    acc_id = mfg_id.get('analytic_account_id')[0]
-                    _add_ana_line()
+            if len(vals.get('mfg_ids')[0][2]) <= 0:
+                raise osv.except_osv(_('Error!'),_('The MFG IDs are required for the work order task\'s working hour!'))
+            if not vals.get('emp_ids')[0][2]:
+                raise osv.except_osv(_('Error!'),_('The Employees are required for the work order task\'s working hour!'))
+        #set the common values
+        vals_line['name'] = '%s: %s' % (tools.ustr(task_data.name), tools.ustr(vals['name'] or '/'))
+        vals_line['user_id'] = vals['user_id']
+        vals_line['date'] = vals['date'][:10]
+        vals_line['unit_amount'] = vals['hours']
+        #get the employee ids
+        emp_ids = vals['emp_ids'][0][2]
+        if not emp_ids:
+            emp_id = emp_obj.search(cr, uid, [('user_id', '=', vals.get('user_id', uid))])
+            if not emp_id:
+                user_name = self.pool.get('res.users').read(cr, uid, [vals.get('user_id', uid)], ['name'])[0]['name']
+                raise osv.except_osv(_('Bad Configuration !'),
+                     _('Please define employee for user "%s". You must create one.')% (user_name,))
+            emp_ids.append(emp_id[0])
+                                    
+        if task_data.project_type != 'mfg':
+            acc_id = task_data.project_id and task_data.project_id.analytic_account_id.id or False
+            for emp_id in emp_ids:
+                _add_ana_line(acc_id, emp_id)
+        else:            
+            #mfg_ids structure is like: 'mfg_ids': [[6, False, [392, 391]]
+            mfg_ids = vals['mfg_ids'][0][2]
+            #split the hours by the count of ID
+            vals_line['unit_amount'] = vals_line['unit_amount']/float(len(mfg_ids))
+            #add ref
+            vals_line['ref'] = '%s : %s'%(task_data.workorder_id.production_id.name, task_data.workorder_id.code)
+            #loop to generate analytic lines by ID list, 
+            for mfg_id in self.pool.get('sale.product').read(cr,uid,mfg_ids,['analytic_account_id'],context=context):
+                if not mfg_id.get('analytic_account_id'):
+                    continue
+                acc_id = mfg_id.get('analytic_account_id')[0]
+                for emp_id in emp_ids:
+                    _add_ana_line(acc_id, emp_id)
     return super(project_timesheet.project_work,self).create(cr, uid, vals, context=context)   
 project_timesheet.project_work.create = _project_timesheet_create     
