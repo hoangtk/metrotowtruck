326833bcdb8e897f1c9c489ee7fb0c5f2dad5c66
John Wang <john.wang@metrospv.com>
2014-10-13 17:29:30 +0800

    When do invoice deletion, also set the linked picking invoice state to '2binvoiced'

--- metro_accounts/account_invoice.py
+++ metro_accounts/account_invoice.py
@@ -12,4 +12,12 @@ class account_invoice(osv.osv):
         self.write(cr, uid, ids, {'internal_number':False})
         return resu
     
+    def unlink(self, cr, uid, ids, context=None):   
+        #get the picking ids first
+        pick_ids = self.pool.get('stock.picking').search(cr,uid,[('invoice_id','in',ids),('invoice_state','=','invoiced')],context=context)
+        #do deletion
+        resu = super(account_invoice,self).unlink(cr, uid, ids, context=context)
+        #update the related picking invoice state
+        self.pool.get('stock.picking').write(cr, uid, pick_ids, {'invoice_state':'2binvoiced'},context=context)
+        return resu
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
