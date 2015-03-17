5b8f837941328e07fcaa1deeaacd0917cba04c4c
John Wang <john.wang@metrospv.com>
2015-03-17 17:28:26 +0800

    怎么查询没有设置图片儿的产品: 因为product_product.image已经不再存储图片,图片存储在文件系统,所以需要增加image_small/medium的fnct_search

--- third_modules/product_image_filestore/product.py
+++ third_modules/product_image_filestore/product.py
@@ -77,14 +77,54 @@ class product_product(orm.Model):
         return self.write(cr, uid, [id], {
             'image': tools.image_resize_image_big(value)}, context=context)
 
-
+    def _have_image_search(self, cr, uid, obj, field_name, args, context=None):
+        '''
+        the image 'is set'/'is not set' searching options
+        @param args: [(u'image_small', u'=', False)]  or [(u'image_small', u'!=', False)] 
+        '''        
+        if not args or args[0][2]:
+            return []
+             
+        #get all product ids
+#        all_ids = self.pool.get('product.product').search(cr, uid, [], context=context)
+        
+        #get product ids with images
+        cr.execute("select distinct res_id from ir_attachment \
+                        where res_model = 'product.product' \
+                        and name in('image.jpg','image_small.jpg','image_medium.jpg')")
+        res = cr.fetchall()
+        #fetchall: [(251,), (2026,), (2409,)]
+        #dictfetchall:[{'res_id': 251}, {'res_id': 2026}]
+        have_image_ids = []
+        if res:
+            have_image_ids = [prod_id for prod_id, in res]
+            
+#        #to reduct the list size that use to search        
+#        have_not_image_ids = []        
+#        if len(have_image_ids) > len(all_ids)/2:
+                    
+        #[(u'image_small', u'=', False)] , the products without images
+        if args[0][1] == '=':
+            if have_image_ids:
+                return [('id', 'not in', have_image_ids)]
+            else:
+                return []
+        #[(u'image_small', u'!=', False)], the products with images
+        if args[0][1] == '!=':
+            if have_image_ids:
+                return [('id', 'in', have_image_ids)]
+            else:
+                #no products with images
+                return [('id', '<', 0)]
+        return []
+    
     _columns = {
-        'image_medium': fields.function(_get_image, fnct_inv=_set_image,
+        'image_medium': fields.function(_get_image, fnct_inv=_set_image, fnct_search=_have_image_search,
             string="Medium-sized image", type="binary", multi="_get_image",
             help="Medium-sized image of the product. It is automatically "\
                  "resized as a 128x128px image, with aspect ratio preserved, "\
                  "only when the image exceeds one of those sizes. Use this field in form views or some kanban views."),
-        'image_small': fields.function(_get_image, fnct_inv=_set_image,
+        'image_small': fields.function(_get_image, fnct_inv=_set_image, fnct_search=_have_image_search,
             string="Small-sized image", type="binary", multi="_get_image",
             help="Small-sized image of the product. It is automatically "\
                  "resized as a 64x64px image, with aspect ratio preserved. "\
