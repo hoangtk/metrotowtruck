c1afd477d830d7e42543335c840694f23c39e62f
John Wang <john.wang@metrospv.com>
2015-04-09 13:57:19 +0800

    Add the i18n fixing module

---
+++ metro_i18n_fix/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Metro i18n fixing',
+    'version': '1.0',
+    'category': 'Metro',
+    "sequence": 14,
+    'complexity': "easy",
+    'description': """
+    
+        This module contains i18n fixing 
+        Metro Modules.
+    
+    """,
+    'author': 'Metro Tower Trucks',
+    'website': 'http://www.metrotowtrucks.com',
+    'depends': ['base'],
+    'init_xml': [],
+    'data' : [],
+    'installable': True,
+    'auto_install': False,
+    'application': True, 
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
