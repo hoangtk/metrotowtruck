0e7dfe15619b10708a29a66a46a7d3d648ac23b2
jack.wang <jack.wang@metrospv.cn>
2015-05-08 15:05:56 +0800

    Add a new option to print out the journal entries.

--- metro_accounts/report/rpt_account_move.py
+++ metro_accounts/report/rpt_account_move.py
@@ -18,7 +18,31 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import rpt_account_move
 
+import time
+
+from openerp.report import report_sxw
+from openerp.addons.metro.rml import rml_parser_ext  
+from metro.utils import cnumber
+
+class account_move(rml_parser_ext):
+    def __init__(self, cr, uid, name, context=None):
+        super(account_move, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'time': time, 
+            'to_cn': self.to_cn, 
+            
+        })
+        
+    def to_cn(self, amount):
+        pt = cnumber()
+        if not isinstance(amount, str):
+            str_amount = str(amount).replace(',', '') #delete the ','
+            cn_amt = pt.cwchange(str_amount)
+        else:
+            cn_amt = pt.cwchange(amount)
+        return cn_amt
+            
+report_sxw.report_sxw('report.account.move.invoice', 'account.move', 'addons/metro_accounts/account_move.rml', parser=account_move, header="internal")
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
